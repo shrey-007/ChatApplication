0f348a931bb4b43f1ce707246d403c0b8b879c0c
shreySaxena <shreyyerhs07@gmail.com>
2024-02-11 02:24:04 +0530

    added swing

--- Client.java
+++ Client.java
@@ -1,9 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client {
+public class Client extends JFrame{
 
     Socket socket;
 
@@ -11,23 +15,70 @@ public class Client {
     PrintWriter writer;
 
 
+    //declare components
+    private JLabel heading=new JLabel("Shrey");
+    private JTextArea messageArea=new JTextArea();
+    private JTextField messageInput=new JTextField();
+
+    private Font font=new Font("Roboto",Font.PLAIN,20);
+
+
     public Client(){
         try{
-            //sending request to server's 7777 port
+//            sending request to server's 7777 port
             this.socket=new Socket("127.0.0.1",7777);
 
 
             reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             writer=new PrintWriter(socket.getOutputStream());
-
-            startReading();
+//
+              createGUI();
+              handleEvents();
+              startReading();
             startWriting();
 
         }
         catch (Exception e){
 
         }
+    }
+
+    private void createGUI(){
+        this.setTitle("ShreyNet");
+        this.setSize(500,500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //coding for component
+        heading.setFont(font);
+        messageArea.setFont(font);
+        messageInput.setFont(font);
+
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+//      heading.setIcon(new ImageIcon("mmvy_status.png"));
+        heading.setHorizontalTextPosition(SwingConstants.CENTER);
+        heading.setVerticalTextPosition(SwingConstants.CENTER);
+
+        messageArea.setEditable(false);
+
+
+        //layout of frame
+        this.setLayout(new BorderLayout());
+
+        //adding components to frame
+        //north=top, center=center,south=bottom
+        this.add(heading,BorderLayout.NORTH);
+        JScrollPane jScrollPane=new JScrollPane(messageArea);
+        this.add(jScrollPane,BorderLayout.CENTER);
+        this.add(messageInput,BorderLayout.SOUTH);
+
+
+
+
+        this.setVisible(true);
+
     }
 
     public void startReading(){
@@ -40,10 +91,13 @@ public class Client {
                         String message = reader.readLine();
                         if (message.equals("exit")) {
                             System.out.println("Server exited the chat");
+                            JOptionPane.showMessageDialog(this,"Server exited the chat");
+                            messageInput.setEnabled(false);
                             socket.close();
                             break;
                         }
-                        System.out.println("Server :" + message);
+//                        System.out.println("Server :" + message);
+                        messageArea.append("Server: "+message+"\n");
                     }
                 }
                 catch (Exception e){
@@ -54,6 +108,35 @@ public class Client {
         };
 
         new Thread(r1).start();
+    }
+
+    private void handleEvents(){
+        messageInput.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                //enter ka key code 10 hota hai toh enter dabate se hi ye chal jaaega
+                if(e.getKeyCode()==10){
+                    String contentToSend=messageInput.getText();
+                    messageArea.append("Me :"+contentToSend+"\n");
+                    writer.println(contentToSend);
+                    writer.flush();
+                    messageInput.setText("");
+                    messageInput.requestFocus();
+
+                }
+
+            }
+        });
     }
 
     public void startWriting(){
